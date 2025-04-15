f15d2025fa13c7857bf61df4779286f7df49cddc
Ajinkya Ganesh Kumbhar <ajinkyakumbhar423@gmail.com>
2025-04-15 07:33:42 +0000

    test

--- uiddemo.go
+++ uiddemo.go
@@ -9,19 +9,18 @@ import (
 	"gitlab.com/rwxrob/uniq"
 )
 
-const defaultHeader = "X-Traefik-UIDDemoID"
-
 // Config the plugin configuration.
 type Config struct {
 	// ...
 	HeaderName string
+	Mystring string
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
 		// ...
-		HeaderName: defaultHeader,
+		HeaderName: "X-Traefik-UIDDemoID",
 	}
 }
 
@@ -30,6 +29,7 @@ type UIDDemo struct {
 	next       http.Handler
 	headerName string
 	name       string
+	mystring   string
 	// ...
 }
 
@@ -44,13 +44,17 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 		next:       next,
 		headerName: config.HeaderName,
 		name:       name,
+		mystring    config.Mystring,
 	}, nil
 }
 
 func (u *UIDDemo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	uid := uniq.UUID()
 	req.Header.Set(u.headerName, uid)
+	req.Header.Set(u.mystring, "thi")
 	rw.Header().Set(u.headerName, uid)
+
+	rw.Header().Set(u.mystring, "this")
 	// rw.Write([]byte("Hello you"))
 	u.next.ServeHTTP(rw, req)
 }
